932220a05b5461055b687bf20f80488be0d8543c
Aleksandr Morozov <moroz.cuporos@gmail.com>
2023-12-19 16:47:09 +0300

    Lesson-5-reshenie-2 full, no ya bi tak ne bil uveren

--- my-app/src/components/todo/todo.js
+++ my-app/src/components/todo/todo.js
@@ -1,15 +1,42 @@
+import { Button } from '../button/button';
 import styles from './todo.module.css';
 
-export const Todo = ({ title, completed }) => {
+export const Todo = ({
+	title,
+	completed,
+	isEditing,
+	onEdit,
+	onTitleChange,
+	onCompletedChange,
+	onSave,
+	onRemove,
+}) => {
 	return (
 		<div className={styles.todo}>
 			<input
 				className={styles.checkbox}
 				type="checkbox"
 				checked={completed}
-				readOnly
+				onChange={({ target }) => onCompletedChange(target.checked)}
 			/>
-			{title}{' '}
+			<div className={styles.title}>
+				{isEditing ? (
+					<input
+						type="text"
+						value={title}
+						onChange={({ target }) => onTitleChange(target.value)}
+					/>
+				) : (
+					<div onClick={onEdit}>{title}</div>
+				)}
+			</div>
+			<div>
+				{isEditing ? (
+					<Button onClick={onSave}>✎</Button>
+				) : (
+					<Button onClick={onRemove}>✖</Button>
+				)}
+			</div>
 		</div>
 	);
 };
